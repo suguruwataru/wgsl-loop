4e3b5389e3cbf6426b489d776a94313b8ae175a4
suguruwataru <23465321+suguruwataru@users.noreply.github.com>
2025-04-17 12:18:14 +0800

    Create the tests

---
+++ src/lib.rs
@@ -0,0 +1,141 @@
+#[cfg(test)]
+mod test {
+    // ONLY TO BE RUN WITH `cargo test -- --test-threads=1`
+    use std::sync::atomic::{AtomicU32, Ordering};
+    use pollster::FutureExt;
+
+    static LOCK: AtomicU32 = AtomicU32::new(0);
+    static mut CTR: u32 = 0;
+
+    #[test]
+    fn loop_atomic() {
+        unsafe { CTR = 0 }
+        (0..2)
+            .map(|id| {
+                let t = move || loop {
+                    if id == 0 {
+                        std::thread::sleep(std::time::Duration::from_millis(100))
+                    }
+                    if LOCK.fetch_or(1, Ordering::Relaxed) == 1 {
+                        continue;
+                    }
+                    unsafe { CTR += 1 }
+                    LOCK.fetch_and(0, Ordering::Relaxed);
+                    break;
+                };
+                std::thread::spawn(t)
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .for_each(|h| h.join().unwrap());
+        assert_eq!(unsafe { CTR }, 2);
+    }
+
+    #[test]
+    fn while_atomic() {
+        unsafe { CTR = 0 }
+        (0..2)
+            .map(|id| {
+                let t = move || loop {
+                    if id == 0 {
+                        std::thread::sleep(std::time::Duration::from_millis(100))
+                    }
+                    while LOCK.fetch_or(1, Ordering::Relaxed) == 1 {}
+                    unsafe { CTR += 1 }
+                    LOCK.fetch_and(0, Ordering::Relaxed);
+                    break;
+                };
+                std::thread::spawn(t)
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .for_each(|h| h.join().unwrap());
+        assert_eq!(unsafe { CTR }, 2);
+    }
+
+    #[ignore = "this makes your computer hang"]
+    #[test]
+    fn wgsl_while() {
+        run_shader("while_atomic");
+    }
+
+    #[test]
+    fn wgsl_loop() {
+        run_shader("loop_atomic");
+    }
+
+    fn run_shader(entry_point: &str) {
+        let wgpu_instance = wgpu::Instance::default();
+        let adapter = wgpu_instance
+            .request_adapter(&wgpu::RequestAdapterOptions::default())
+            .block_on()
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                required_features: wgpu::Features::TEXTURE_ADAPTER_SPECIFIC_FORMAT_FEATURES,
+                ..Default::default()
+            })
+            .block_on()
+            .unwrap();
+        device.on_uncaptured_error(Box::new(|error| {
+            println!("{}", error);
+            panic!();
+        }));
+        let shader_module = device.create_shader_module(wgpu::include_wgsl!("loop.wgsl"));
+        let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            module: &shader_module,
+            label: None,
+            layout: None,
+            entry_point: Some(entry_point),
+            compilation_options: wgpu::PipelineCompilationOptions::default(),
+            cache: None,
+        });
+        use wgpu::util::DeviceExt;
+        let src_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: None,
+            contents: &[0u8; 8],
+            usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::STORAGE,
+        });
+        let dst_buf = device.create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            size: 8,
+            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+            mapped_at_creation: false,
+        });
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
+        {
+            let mut pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor::default());
+            let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+                label: None,
+                layout: &pipeline.get_bind_group_layout(0),
+                entries: &[wgpu::BindGroupEntry {
+                    resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                        buffer: &src_buf,
+                        size: Some(4.try_into().unwrap()),
+                        offset: 0,
+                    }),
+                    binding: 0,
+                }],
+            });
+            pass.set_bind_group(0, &bind_group, &[]);
+            pass.set_pipeline(&pipeline);
+            pass.dispatch_workgroups(1, 1, 1);
+        }
+        encoder.copy_buffer_to_buffer(&src_buf, 0, &dst_buf, 0, 4);
+        let (sender, receiver) = std::sync::mpsc::channel();
+        let idx = queue.submit([encoder.finish()]);
+        device
+            .poll(wgpu::PollType::WaitForSubmissionIndex(idx))
+            .unwrap();
+        dst_buf.slice(..).map_async(wgpu::MapMode::Read, move |r| {
+            r.unwrap();
+            sender.send(()).unwrap();
+        });
+        device.poll(wgpu::PollType::Wait).unwrap();
+        receiver.recv().unwrap();
+        assert_eq!(
+            2,
+            u32::from_ne_bytes((*dst_buf.get_mapped_range(..))[..4].try_into().unwrap())
+        );
+    }
+}
---
+++ src/loop.wgsl
@@ -0,0 +1,20 @@
+var<workgroup> lock: atomic<u32>;
+@group(0) @binding(0)
+var<storage, read_write> mark: u32;
+
+@compute @workgroup_size(2, 1, 1)
+fn loop_atomic(@builtin(global_invocation_id) id: vec3u) {
+    loop {
+        if atomicOr(&lock, 1u) == 1u { continue; }
+        mark++;
+        atomicAnd(&lock, 0u);
+        break;
+    }
+}
+
+@compute @workgroup_size(2, 1, 1)
+fn while_atomic(@builtin(global_invocation_id) id: vec3u) {
+    while atomicOr(&lock, 1u) == 1u { }
+    mark++;
+    atomicAnd(&lock, 0u);
+}
